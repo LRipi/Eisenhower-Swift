f0bc2f306faf1c876f396f94879710a34cf7172e
Léo Riberon-Piatyszek <leo.riberon-piatyszek@epitech.eu>
2020-01-14 15:45:14 +0100

    Init project

---
+++ eisenhower/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eisenhower
+//
+//  Created by Léo Riberon-Piatyszek on 14/01/2020.
+//  Copyright © 2020 epitech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
